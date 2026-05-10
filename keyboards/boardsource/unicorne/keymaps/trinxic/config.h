a626798481f3f41f3138641ace2bcdba30bced25
Trinxic <davida_2016@outlook.com>
2026-05-10 15:47:53 -0400

    fixed no-song definition

--- keyboards/boardsource/unicorne/keymaps/trinxic/config.h
+++ keyboards/boardsource/unicorne/keymaps/trinxic/config.h
@@ -13,6 +13,7 @@
 #define AUDIO_INIT_DELAY
 #define AUDIO_PWM_CHANNEL RP2040_PWM_CHANNEL_A
 // #define STARTUP_SONG SONG(ODE_TO_JOY)  // AHHHHHHHH
+#undef STARTUP_SONG
 #define STARTUP_SONG SONG(NO_SOUND) // finally, peace!
 
 // Analog Sticks (I don't have these)
